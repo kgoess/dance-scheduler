7a106bc355fd077e2c3071bc65e19e72a254c838
Kevin M. Goess <kevin@goess.org>
2022-03-23 18:58:41 -0700

    Add foreign keys to band_membership schema

--- schemas/band_membership.sql
+++ schemas/band_membership.sql
@@ -3,5 +3,12 @@ CREATE TABLE band_membership (
     band_id INT NOT NULL,
     talent_id INT NOT NULL,
 
+    FOREIGN KEY (band_id)
+        REFERENCES bands(band_id)
+        ON DELETE RESTRICT,
+    FOREIGN KEY (talent_id)
+        REFERENCES talent(talent_id)
+        ON DELETE RESTRICT,
+
     UNIQUE INDEX band_membership_idx(band_id, talent_id)
 );
