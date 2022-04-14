5abd604338a76f2d8cedafbab1c2488313a8beff
Kevin M. Goess <kevin@goess.org>
2022-04-13 19:50:54 -0700

    Implement basic editing
    The submit button works, just need a backend

--- public/javascripts/scheduler.js
+++ public/javascripts/scheduler.js
@@ -1,7 +1,7 @@
 
  $( document ).ready(function() {
 
-  $( '#load-event-button' ).click(function() {
+ $( '#load-event-button' ).click(function() {
     const eventId = $( '#event-id-input' )[0].value;
     $.ajax({
       url: `event/${eventId}`,
@@ -25,15 +25,40 @@
     .done( (msg) => { displayEvent(msg) });
   });
 
-}); 
+    $( '.event-display-row' ).click(function() {
+        const display = $(this).children(".row-contents")[0];
+        if (display.hidden) {
+            return;
+        }
+        display.hidden = !display.hidden;
+        const edit  = $(this).children(".row-edit")[0];
+        edit.hidden = !edit.hidden;
+    });
 
+    $( '#event-save-button' ).click(function() {
+        const eventId = $( '#event-display [name="event_id"]' )[0].value;
+        $.ajax({
+            url: `event/${eventId}`,
+            dataType: "json",
+            method: "post"
+        })
+        .done( (msg) => { displayEvent(msg) })
+        .fail( () => { alert("something bad happened, update failed") }); // FIXME later
+
+
+    });
+
+
+}); 
 
 function displayEvent(eventObj) {
   $( '#event-display .event-display-row' ).each(
     function(index) {
       $(this).children(".row-contents")[0].textContent = eventObj[this.getAttribute("name")];
+      $(this).children(".row-edit")[0].value = eventObj[this.getAttribute("name")];
     }
   );
+  $( '#event-display [name="event_id"]' )[0].value = eventObj["event_id"]; 
   $( '#event-display' ).show();
 }
 
