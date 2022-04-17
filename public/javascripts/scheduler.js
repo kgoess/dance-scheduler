acf55430b530fc8f429e27f4ab81757e46c4ce8c
Kevin M. Goess <kevin@goess.org>
2022-04-16 17:21:44 -0700

    Hook up create event button in UI

--- public/javascripts/scheduler.js
+++ public/javascripts/scheduler.js
@@ -35,52 +35,82 @@
     $( '#event-display-form' ).submit(function(e){
         e.preventDefault();
     });
+
     $( '#event-save-button' ).click(function() {
         const eventId = $( '#event-display [name="event_id"]' )[0].value;
         const dataString = $( '#event-display-form' ).serialize();
+        let http_method;
+        let url;
+        if (eventId) {
+            http_method = "put";
+            url = `event/${eventId}`
+        } else {
+            http_method = "post";
+            url = "event/";
+        }
         $.ajax({
-            url: `event/${eventId}`,
+            url,
             dataType: "json",
-            method: "post",
+            method: http_method,
             data: dataString
         })
         .done( (msg) => { 
             loadEventList();
-            $.ajax({
-              url: `event/${eventId}`,
-              dataType: "json"
-            })
-            .done( (msg) => { displayEvent(msg) });
+            displayEvent(msg);
          })
         .fail( () => { alert("something bad happened, update failed") }); // FIXME later
     });
 
-
+    $( '#event-create-button' ).click(function() {
+        $( '#event-display .event-display-row' ).each(
+            function(index) {
+                $(this).children(".row-contents")[0].hidden = true;
+                $(this).children(".row-contents")[0].textContent = "";
+                $(this).children(".row-edit")[0].hidden = false;
+                $(this).children(".row-edit")[0].value = "";
+            }
+        );
+        $( '#event-display [name="event_id"]' )[0].value = "";
+        $( '#event-display' )[0].hidden = false;
+    });
 }); 
 
-function displayEvent(eventObj) {
-  $( '#event-display .event-display-row' ).each(
-    function(index) {
-      $(this).children(".row-contents")[0].hidden = false;
-      $(this).children(".row-contents")[0].textContent = eventObj[this.getAttribute("name")];
-      $(this).children(".row-edit")[0].hidden = true;
-      $(this).children(".row-edit")[0].value = eventObj[this.getAttribute("name")];
+function displayEvent(msg) {
+
+    if (msg["data"]) {
+        const eventObj = msg["data"];
+        $( '#event-display .event-display-row' ).each(
+            function(index) {
+                $(this).children(".row-contents")[0].hidden = false;
+                $(this).children(".row-contents")[0].textContent = eventObj[this.getAttribute("name")];
+                $(this).children(".row-edit")[0].hidden = true;
+                $(this).children(".row-edit")[0].value = eventObj[this.getAttribute("name")];
+            }
+        );
+        $( '#event-display [name="event_id"]' )[0].value = eventObj["event_id"]; 
+        $( '#event-display' )[0].hidden = false;
+    } else {
+        alert("error: " + msg["error"]);
     }
-  );
-  $( '#event-display [name="event_id"]' )[0].value = eventObj["event_id"]; 
-  $( '#event-display' )[0].hidden = false;
 }
 
-function displayEventList(events) {
+function displayEventList(msg) {
 
     let insertTarget = $( "#all-events-list" );
 
-    events.forEach((event, i) => {
-        let element = document.createElement('option');
-        element.innerHTML = event["name"];
-        element.setAttribute("value", event["event_id"]);
-        insertTarget.append(element);
-    });
+    if (msg["data"]) {
+
+        const events = msg["data"];
+
+        events.forEach((event, i) => {
+            let element = document.createElement('option');
+            element.innerHTML = event["name"];
+            element.setAttribute("value", event["event_id"]);
+            insertTarget.append(element);
+        });
+    } else {
+        alert("fail: " + msg["error"]);
+    }
 }
 
 function loadEventList() {
