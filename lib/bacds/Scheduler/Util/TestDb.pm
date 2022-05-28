6473afaf5defe00bc4fc2dffba70792f21ee170f
Kevin M. Goess <kevin@goess.org>
2022-05-28 10:04:09 -0700

    Make testdb's independent
    So you can run tests in parallel à la
    
        prove -Ilib -j 4 t/

--- lib/bacds/Scheduler/Util/TestDb.pm
+++ lib/bacds/Scheduler/Util/TestDb.pm
@@ -3,13 +3,21 @@ package bacds::Scheduler::Util::TestDb;
 use 5.16.0;
 use warnings;
 
+use File::Basename qw/basename/;
+
 use Exporter 'import';
 our @EXPORT_OK = qw/setup_test_db/;
 
 
 sub setup_test_db{
-    unlink "testdb";
-    $ENV{TEST_DSN} = 'dbi:SQLite:dbname=testdb';
+
+    my $test_script = basename $0, '.t';
+
+    my $test_db = "testdb.$test_script.sqlite";
+
+    unlink $test_db;
+
+    $ENV{TEST_DSN} = "dbi:SQLite:dbname=$test_db";
     # load an on-disk test database and deploy the required tables
     bacds::Scheduler::Schema->connection($ENV{TEST_DSN},'','');
     bacds::Scheduler::Schema->load_namespaces;
