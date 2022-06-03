f8932dbb5d5bb04db423dc866e37b4da0409e3b2
Kennith Grotjohn <kwgrotjohn@gmail.com>
2022-06-02 19:47:12 -0700

    Add Band.pm

---
+++ lib/bacds/Scheduler/Model/Band.pm
@@ -0,0 +1,47 @@
+=head1 NAME
+
+bacds::Scheduler::Model::Band - A group of talent with a collective name.
+
+=head1 SYNOPSIS
+
+    my $band = $class->get_row($band_id);
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
+package bacds::Scheduler::Model::Band;
+
+use 5.16.0;
+use warnings;
+
+use parent 'bacds::Scheduler::Model';
+
+sub get_model_name { 'Band' }
+sub get_other_table_names { }
+sub get_fields_for_output {
+    qw/
+        band_id
+        name
+        is_deleted
+        created_ts
+        modified_ts
+    /
+}
+sub get_fields_for_input {
+    qw/
+        band_id
+        name
+        is_deleted
+    /
+}
+sub get_fkey_fields { }
+sub get_relationships { 
+    [qw/Talent talent talent_id/],
+}
+sub get_one_to_manys { }
+sub get_default_sorting { {-asc=>'name'} }
+
+1;
