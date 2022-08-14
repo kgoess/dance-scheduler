a47b7c91823907cff4efba5f3281174b225deee2
Kevin M. Goess <kevin@goess.org>
2022-08-14 16:57:07 -0700

    Add series.sidebar notes
    Not sure we want to just dump all that HTML into the "sidebar" column, discuss?

--- bin/migrate-series.pl
+++ bin/migrate-series.pl
@@ -22,6 +22,13 @@ sub create_series {
             frequency => 'first, third and fifth Wednesdays',
             series_url => 'https://bacds.org/series/contra/berkeley_wed/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./contra/berkeley_wed/content.html:
+<!--#include virtual="/series/shared/role_name_lr.html" -->
+<!--#include virtual="/series/shared/location_gnc.html" -->
+<!--#include virtual="/series/shared/time_late_150.html" -->
+<!--#include virtual="/series/shared/price_weeknight.html" -->
+<!--#include virtual="/series/shared/directions_gnc.html" -->
+EOL
         },
         {
             name => 'San Francisco Contra',
@@ -34,63 +41,129 @@ sub create_series {
             frequency => 'second, fourth and fifth Saturdays',
             series_url => 'https://bacds.org/series/contra/palo_alto/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./contra/palo_alto/content.html:
+<!--#include virtual="/series/shared/role_name_lr.html" -->
+<!--#include virtual="/series/shared/location_pa.html" -->
+<!--#include virtual="/series/shared/time_late_lesson_180.html" -->
+<!--#include virtual="/series/shared/price_weekend.html" -->
+<!--#include virtual="/series/shared/directions_pa.html" -->
+EOL
         },
         {
             name => 'Palo Alto English',
             frequency => 'first, third, and fifth Fridays',
             series_url => 'https://bacds.org/series/english/peninsula/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/peninsula/content.html:
+<!--#include virtual="/series/shared/location_ast.html" -->
+<!--#include virtual="/series/shared/time_mid_120.html" -->
+<!--#include virtual="/series/shared/price_standard.html" -->
+<!--#include virtual="/series/shared/directions_jls.html" -->
+<!--#include virtual="/series/shared/directions_ast.html" -->
+EOL
         },
         {
             name => 'South Bay Contra',
             frequency => 'third Sunday afternoons, 3:00–6:00, lesson at 2:30',
             series_url => 'https://bacds.org/series/contra/san_jose/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./contra/san_jose/content.html:
+<!--#include virtual="/series/shared/location_fsj.html" -->
+<!--#include virtual="/series/shared/time_aft_180_3.html" -->
+<!--#include virtual="/series/shared/price_weekend_sbc.html" -->
+<!--#include virtual="/series/shared/directions_fsj.html" -->
+EOL
         },
         {
             name => 'Hayward Contra',
             frequency => 'fourth and fifth Sunday afternoons of each month, 4:00–7:00, lesson at 3:30',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/hayward/content.html:
+<!--#include virtual="/series/shared/location_hayward.html" -->
+<!--#include virtual="/series/shared/time_aft_180_4.html" -->
+EOL
         },
         {
             name => 'Peninsula English (Palo Alto)',
             frequency => 'first, third, and fifth Tuesday evenings of each month (with a break from late June through Labor Day).',
             series_url => 'https://bacds.org/series/english/palo_alto/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/palo_alto/content.html:
+<!--#include virtual="/series/shared/location_mt.html" -->
+<!--#include virtual="/series/shared/time_late_lesson_150.html" -->
+<!--#include virtual="/series/shared/price_weekend.html" -->
+<!--#include virtual="/series/shared/directions_mt.html" -->
+EOL
         },
         {
             name => 'Berkeley English',
             frequency => 'second and fourth Wednesday',
             series_url => 'https://bacds.org/series/english/berkeley_wed/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/berkeley_wed/content.html:
+<!--#include virtual="/series/shared/time_special_bwe.html" -->
+<!--#include virtual="/series/shared/location_gnc.html" -->
+<!--#include virtual="/series/shared/price_weeknight.html" -->
+<!--#include virtual="/series/shared/directions_gnc.html" -->
+EOL
         },
         {
             name => 'Berkeley Fourth Saturday Experienced Dance',
             frequency => 'fourth Saturday',
             series_url => 'https://bacds.org/series/english/berkeley_sat/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/berkeley_sat/content.html:
+<!--#include virtual="/series/shared/location_gnc.html" -->
+<!--#include virtual="/series/shared/time_mid_150.html" -->
+<!--#include virtual="/series/shared/price_weekend.html" -->
+<!--#include virtual="/series/shared/directions_gnc.html" -->
+EOL
         },
         {
             name => 'San Francisco English',
             frequency => 'second Saturday',
             series_url => 'https://bacds.org/series/english/san_francisco/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/san_francisco/content.html:
+<!--#include virtual="/series/shared/location_spc.html" -->
+<!--#include virtual="/series/shared/time_special_spc.html" -->
+<!--#include virtual="/series/shared/price_weekend.html" -->
+<!--#include virtual="/series/shared/directions_spc.html" -->
+EOL
         },
         {
             name => 'San Jose English',
             frequency => 'second Sunday afternoons of the month, September through June',
             series_url => 'https://bacds.org/series/english/san_jose/',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./english/san_jose/content.html:
+<!--#include virtual="/series/shared/location_fsj.html" -->
+<!--#include virtual="/series/shared/time_aft_150_3.html" -->
+<!--#include virtual="/series/shared/price_weekend.html" -->
+<!--#include virtual="/series/shared/directions_fsj.html" -->
+EOL
         },
         {
             name => 'Atherton Woodshed',
             frequency => 'second Tuesday',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./woodshed/atherton/content.html:
+<!--#include virtual="/series/shared/location_hpp.html" -->
+<!--#include virtual="/series/shared/time_late_120.html" -->
+<!--#include virtual="/series/shared/price_woodshed.html" -->
+<!--#include virtual="/series/shared/directions_hpp.html" -->
+EOL
         },
         {
             name => 'English Ceilidh',
             frequency => 'occasional',
             is_deleted => 0,
+            sidebar => <<'EOL', # ./ceilidh/oakland/content.html:
+<!--#include virtual="/series/shared/location_hum.html" -->
+<!--#include virtual="/series/shared/time_mid.html" -->
+<!--#include virtual="/series/shared/price_weekend.html" -->
+<!--#include virtual="/series/shared/directions_hum.html" -->
+EOL
         },
         {
             name => 'Odd Sundays Online',
@@ -134,6 +207,7 @@ sub create_series {
             name
             frequency
             series_url
+            sidebar
         /;
         $new->is_deleted(0);
 
