kgoess/dance-scheduler

commit fa685019c1cadc49b6f5b7cd4152572d5646234e

Return JSON for 401

lib/bacds/Scheduler/Plugin/Auth.pm
@@ -10,6 +10,7 @@ use HTTP::Negotiate qw/choose/;
 
 use bacds::Scheduler::FederatedAuth;
 use bacds::Scheduler::Util::Cookie qw/LoginMethod LoginSession GoogleToken/;
+use bacds::Scheduler::Util::Results;
 
 =head2 requires_login
 
@@ -34,6 +35,7 @@ plugin_keywords requires_login => sub {
                 ['json', 1.000,    'application/json'],
             ];
             my $media_type = choose($variants, $self->app->request->headers);
+            print STDERR "**********" . $media_type . "\n";
             if ($media_type eq 'html') {
                 return $plugin->login_redirect_html($self);
             } else {
@@ -48,16 +50,31 @@ plugin_keywords requires_login => sub {
 sub login_redirect_html {
     my ($plugin, $app) = @_;
 
-    $app->redirect('/signin.html' => 303);
+    $app->redirect($plugin->get_login_page_url($app) => 303);
 }
 
 sub login_redirect_json {
-    my ($plugin, $app) = @_;
+    my ($plugin, $app, @args) = @_;
+    my $results = bacds::Scheduler::Util::Results->new;
+
+    $results->add_error(40101, "You are not logged in");
+    $results->data(
+        {
+            url => $plugin->get_login_page_url($app),
+        }
+    );
 
     # this 401 will trigger this in the js app:
     #     alert("You don't have authorization to do that!");
     $app->response->status(401);
+    $app->response->headers->content_type('application/json');
+    $app->response->content($results->format);
     # FIXME how to send back the /signin.html location?
+}
+
+sub get_login_page_url {
+    my ($plugin, $app) = @_;
+    return $app->request->uri_base . "/signin.html";
 }
 
 =head2 can_edit_event

lib/bacds/Scheduler/Util/Results.pm
@@ -7,10 +7,12 @@ bacds::Scheduler::Util::Results - Package to collect any errors and format the r
     my $results = bacds::Scheduler::Util::Results->new;
 
     $results->data($array_pointer);
+    # or
+    $results->add_error(4300, "Insert failed for new programmer");
     
     return $results->format;
 
-    outputs JSON like:
+    # outputs JSON like:
     "data": [
         ...
     ],

t/400-auth.t
@@ -10,7 +10,7 @@ use FindBin qw/$Bin/;
 use HTTP::Request::Common;
 use JSON::MaybeXS qw/decode_json/;
 use Plack::Test;
-use Test::More tests => 40;
+use Test::More tests => 44;
 use Test::Warn;
 
 use bacds::Scheduler;
@@ -181,7 +181,6 @@ sub test_requires_login {
 
     my $test_noauth = get_tester();
     my $test_authed = get_tester(auth => 1);
-
     #
     # test GET /
     #
@@ -189,7 +188,22 @@ sub test_requires_login {
     $test_noauth->get('/');
     ok ! $test_noauth->success, 'noauth GET / fails';
     is $test_noauth->res->code, 303, 'noauth GET / gets a 303';
-    is $test_noauth->res->header('location'), '/signin.html', 'noauth GET / right loc';
+    is $test_noauth->res->header('location'), 'http://localhost/signin.html', 'noauth GET / right loc';
+
+    #
+    # test GET JSON /
+    #
+    $test_noauth->add_header(accept=>'application/json');
+    $test_noauth->get('/');
+    ok ! $test_noauth->success, 'noauth GET JSON / fails';
+    is $test_noauth->res->headers->content_type, 'application/json', 'noauth GET JSON returns application/json';
+    is $test_noauth->res->code, 401, 'noauth GET JSON / gets a 401';
+    my $json = decode_json($test_noauth->res->content);
+    is_deeply $json, 
+        {
+          data   => { url => "http://localhost/signin.html" },
+          errors => [{ msg => "You are not logged in", num => 40101 }],
+        };
 }
 
 sub test_can_edit {

commit 3e0d5edc9ab5c65d4687882ff6ca9a43f24eaa7b

Implement the is_superuser check in can_edit_event
That makes the 2xx tests pass.

002_index_route was failing because in fa685019c1cadc we added this so
that it works in our :5000 test environment and in production.

+    $app->redirect($plugin->get_login_page_url($app) => 303);

Weird though, I thought it was passing since then.

The 1xx tests were failing since we added the requires_login to all the
POST endpoints in 6702fa2e2b

Tests are passing now.

lib/bacds/Scheduler/Plugin/Auth.pm
@@ -92,10 +92,13 @@ plugin_keywords can_edit_event => sub {
 
         my $dbh = get_dbh();
 
-        my @rs = $dbh->resultset('Programmer')->search({
+        my $programmer = $dbh->resultset('Programmer')->search({
             email => $email,
-        });
-        my $programmer = $rs[0];
+        })->first();
+
+        if ($programmer->is_superuser) {
+            return $route_sub->(@args);
+        }
 
         my $event_id = $self->app->request->param('event_id');
         my $event_rs = $dbh->resultset('Event')->find($event_id);

t/002_index_route.t
@@ -16,7 +16,7 @@ my $test        = get_tester(auth => 1);
 $test_noauth->get('/');
 is $test_noauth->res->code, '303',
     'unauthenticated request to / returns 303';
-$test_noauth->header_is(location => '/signin.html',
+$test_noauth->header_is(location => 'http://localhost/signin.html',
     'unauthenticated request to / returns redirect to signin.html');
 
 $test->get_ok('/', 'authenticated request ok')

t/100_styles.t
@@ -16,11 +16,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my $Style;

t/101_venue.t
@@ -17,11 +17,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my ($Venue, $Venue_Id);

t/102_series.t
@@ -17,11 +17,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my ($Series, $Series_Id);

t/103_bands.t
@@ -16,11 +16,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my $Band;

t/104_talent.t
@@ -16,11 +16,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my $Talent;

t/105_callers.t
@@ -16,11 +16,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my $Caller;

t/106_parent_orgs.t
@@ -16,11 +16,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/setup_test_db/;
+use bacds::Scheduler::Util::Test qw/setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my $ParentOrg;

t/107_programmers.t
@@ -16,11 +16,11 @@ use bacds::Scheduler::Schema;
 use bacds::Scheduler::Util::Time qw/get_now/;
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-use bacds::Scheduler::Util::Test qw/get_tester setup_test_db/;
+use bacds::Scheduler::Util::Test qw/get_tester setup_test_db get_tester/;
 setup_test_db;
 
 my $app = bacds::Scheduler->to_app;
-my $test = Plack::Test->create($app);
+my $test = get_tester(auth => 1);
 my $dbh = get_dbh();
 
 my $Programmer;