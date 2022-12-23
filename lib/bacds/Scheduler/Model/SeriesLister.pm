e3d319a55fbb56c1d2f1b842e04e48665df6e796
Kevin M. Goess <kevin@goess.org>
2022-12-22 21:38:26 -0800

    Generate ld+json for single event output page.

--- lib/bacds/Scheduler/Model/SeriesLister.pm
+++ lib/bacds/Scheduler/Model/SeriesLister.pm
@@ -25,6 +25,8 @@ use warnings;
 
 use Carp;
 use Data::Dump qw/dump/;
+use Dancer2::Serializer::JSON qw/to_json/;
+use Encode qw/decode_utf8/;
 
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 use bacds::Scheduler::Util::Time qw/get_now/;
@@ -98,7 +100,141 @@ sub get_event_details {
     return {
         series => $event->series,
         events => [$event],
+        ldjson => generate_ldjson($event),
     }
+}
+
+
+sub generate_ldjson {
+    shift if $_[0] eq __PACKAGE__;
+    my ($event) = @_;
+
+    my $date = $event->start_date->ymd;
+    my $venue = $event->venues->first;
+    my $leaders = join ', ', map $_->name, $event->callers;
+    my $bands = join ', ', map $_->name, $event->bands;
+
+    my $start_time = $event->start_time
+        ? $event->start_time
+        : '00:00:00';
+    my $end_time = $event->end_time
+        ? $event->end_time
+        : '00:00:00';
+
+    my $comments = $event->short_desc;
+
+    state $ok_dance_types = { map { $_ => 1 } qw/
+        REGENCY
+        FAMILY
+        ENGLISH
+        CONTRA
+        CEILIDH
+    /};
+    my $nice_dance_type = join '/', map ucfirst lc, grep $ok_dance_types->{$_}, map $_->name, $event->styles;
+    $nice_dance_type //= 'English Country'; # likely to be correct :-/
+
+    #my $offer_url = "https://$ENV{HTTP_HOST}$ENV{DOCUMENT_URI}";
+    my $offer_url = bacds::Scheduler->ssi_uri_for({event_id => $event->event_id});
+
+    my $start = join 'T', $date, $start_time;
+    my $end   = join 'T', $date, $end_time;
+
+
+    # new fields eventAttendanceMode: could be Offline, Online or Mixed
+    # new field eventStatus could be:
+    # 		EventCancelled
+    # 		EventMovedOnline
+    # 		EventPostponed
+    # 		EventRescheduled
+    # 		EventScheduled
+    # just hard-coding them to start with
+
+    my %json_data = (
+       '@context'  => 'http://schema.org',
+       '@type'    => ['Event','DanceEvent'],
+        name        => "$nice_dance_type Dancing, calling by $leaders to the music of $bands",
+        startDate   => $start,
+        endDate     => $end,
+        eventAttendanceMode => 'https://schema.org/OfflineEventAttendanceMode',
+        eventStatus => 'https://schema.org/EventScheduled',
+        organizer => {
+           '@context' => 'http://schema.org',
+           '@type'    => 'Organization',
+            name      => 'Bay Area Country Dance Society',
+            url       => 'https://www.bacds.org/'
+        },
+        location => {
+           '@context' => 'http://schema.org',
+           '@type'    => 'Place',
+            name      => $venue->hall_name,
+            address => {
+               '@type'          => "PostalAddress",
+                streetAddress   => $venue->address,
+                addressLocality => $venue->city,
+                postalCode      => $venue->zip,
+                addressRegion   => "California",
+                addressCountry  => "USA"
+            }
+        },
+        description => "$nice_dance_type Dancing at ".$venue->hall_name." in ".$venue->city.". ".
+            "Everyone welcome, beginners and experts! No partner necessary. ".
+            "$comments (Prices may vary for special events or workshops. Check ".
+            "the calendar or website for specifics.)",
+        image => 'https://www.bacds.org/graphics/bacdsweblogomed.gif',
+        performer => [
+            {
+               '@type' => 'MusicGroup',
+                name   => $bands,
+            },
+            {
+               '@type' => 'Person',
+                name => $leaders,
+            }
+        ],
+        offers => [
+        {
+            '@type' => "Offer",
+            name => "supporters",
+            price => "25.00",
+            priceCurrency => "USD",
+            url => $offer_url,
+            availability => "http://schema.org/LimitedAvailability",
+            validFrom  => $date,
+        },{
+            '@type' => 'Offer',
+            name => 'non-members',
+            price => '20.00',
+            priceCurrency => 'USD',
+            url => $offer_url,
+            availability => 'http://schema.org/LimitedAvailability',
+            validFrom => $date,
+        },{
+            '@type' => 'Offer',
+            name => 'members',
+            price => '15.00',
+            priceCurrency => 'USD',
+            url => $offer_url,
+            availability => 'http://schema.org/LimitedAvailability',
+            validFrom => $date,
+        },{
+            '@type' => 'Offer',
+            name => 'students or low-income or pay what you can',
+            price => '6.00',
+            priceCurrency => 'USD',
+            url => $offer_url,
+            availability => 'http://schema.org/LimitedAvailability',
+            validFrom => $date
+        }
+        ]
+    );
+
+    # decode to logical characters, which is what the template expects
+    my $json_str = decode_utf8 Dancer2::Serializer::JSON::to_json(\%json_data, { pretty => 1 });
+    return <<EOL;
+    <script type="application/ld+json">
+    $json_str
+    </script>
+EOL
 }
 
 1;
