8651fe9bec60c117f4140d127ee15a4d19191ee3
Kevin M. Goess <kevin@goess.org>
2025-02-09 15:58:29 -0800

    Only load the calendar if there's a CalendarEl
    so that the javascript works on things other than the site front page,
    like for instance the individual series pages.

--- public/js/unearth/bacds.js
+++ public/js/unearth/bacds.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
+  if (calendarEl) {
+      var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: "dayGridMonth",
 
-    headerToolbar: { center: "dayGridMonth,timeGridWeek" }, // buttons for switching between views
+        headerToolbar: { center: "dayGridMonth,timeGridWeek" }, // buttons for switching between views
 
-    defaultTimedEventDuration: "02:30",
-    events: {
-      url: "/dance-scheduler/livecalendar-results",
-      failure: function () {
-        alert("there was an error while fetching events!");
-      },
-    },
-    eventDisplay: "block",
+        defaultTimedEventDuration: "02:30",
+        events: {
+          url: "/dance-scheduler/livecalendar-results",
+          failure: function () {
+            alert("there was an error while fetching events!");
+          },
+        },
+        eventDisplay: "block",
 
-    eventClick: function (event) {
-      if (event.url) {
-        window.open(event.url);
-        return false;
-      }
-    },
-  });
-  calendar.render();
+        eventClick: function (event) {
+          if (event.url) {
+            window.open(event.url);
+            return false;
+          }
+        },
+      });
+      calendar.render();
+    }
 });
 
 /** multi-level dropdown code from https://stackoverflow.com/a/66470962 */
