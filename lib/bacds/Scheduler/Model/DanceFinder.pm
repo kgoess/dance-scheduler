c76f5f4a0d259cd29b62a8278d90da27fb0f4b7b
Kevin M. Goess <kevin@goess.org>
2025-10-03 09:27:37 -0700

    Reverting "Fix DateTime error in DanceFinder->search_events"
    Reverting 16e229b6cc34a7fc6bea99f6e650e94de9801b75 and
    da28da1b041cb570e8b9e3d27835c10ea091488f. The search for today's events
    was returning nothing because the query was being generated with the
    "time" part of the date string like this:
    
        start_date BETWEEN '2025-10-03 09:08:01' AND '2025-10-04 00:00:00'
    
    and start_date is just "2025-10-03".
    
    Will revisit (and add unit tests).

--- lib/bacds/Scheduler/Model/DanceFinder.pm
+++ lib/bacds/Scheduler/Model/DanceFinder.pm
@@ -19,7 +19,6 @@ use Data::Dump qw/dump/;
 
 use bacds::Scheduler::Util::Db qw/get_dbh/;
 use bacds::Scheduler::Util::Time qw/get_today/;
-use DateTime::Format::ISO8601 qw/parse_datetime/;
 
 =head2 related_entities_for_upcoming_events
 
@@ -167,9 +166,6 @@ sub search_events {
         if %args;
 
     my $dbh = get_dbh(debug => $dbix_debug, db => $db_arg, user => $dbuser_arg);
-    my $dtf = $dbh->storage->datetime_parser;
-    my $parse_datetime = sub{DateTime::Format::ISO8601->parse_datetime(@_)};
-
 
     # These prefetches means all the data is fetched with one big
     # JOIN, you can verify that by adding debug=>1 in the
@@ -195,9 +191,7 @@ sub search_events {
         @$team_arg ? 'event_team_maps' : (),
         @$role_pair_arg ? 'event_role_pairs_maps' : (),
     );
-    my $start_date = $dtf->format_datetime($start_date_arg ? $parse_datetime->($start_date_arg) : get_today());
-
-    
+    my $start_date = $start_date_arg || get_today()->ymd;
 
     my @role_search;
     if ($role_pair_allow_blank){
@@ -223,7 +217,7 @@ sub search_events {
             '-not_bool' => 'is_deleted',
             start_date =>
                 $end_date_arg
-                ? { '-between' => [$start_date, $dtf->format_datetime($parse_datetime->($end_date_arg))] }
+                ? { '-between' => [$start_date, $end_date_arg] }
                 : { '>=' => $start_date },
             @$caller_arg ? ('event_callers_maps.caller_id' => $caller_arg) : (),
             @$venue_arg  ? ('event_venues_maps.venue_id'   => $venue_arg)  : (),
