d20eef90aa01adc756f16520567dfc0228268cda
Kevin M. Goess <kevin@goess.org>
2025-11-24 20:39:38 -0800

    Fix series links on weekly email
    When we were working on 3c7fcdc6 last week, fixing up the canonical
    links for series, we were focused on the web links, and while we did get
    as far as adding the canonical_scheme and canonical_host to
    send-weekly-schedule.pl, we forgot to later remove the "://" from the
    scheme and we completely neglected after updating the url formulations
    in unearth/listings.tt to update the similar bits in
    dancefinder/listings.tt

--- bin/send-weekly-schedule.pl
+++ bin/send-weekly-schedule.pl
@@ -75,7 +75,7 @@ my $today_str = DateTime->now->strftime("%a., %b. %e");
 
 my ($html_part, $text_part);
 $tt->process('send-weekly-schedule/html.tt' => {
-    canonical_scheme => 'https://',
+    canonical_scheme => 'https',
     canonical_host => 'bacds.org',
     events => \@events,
     highlight_special_types => 1,
@@ -83,7 +83,7 @@ $tt->process('send-weekly-schedule/html.tt' => {
 }, \$html_part) || die $tt->error;
 
 $tt->process('send-weekly-schedule/text.tt' => {
-    canonical_scheme => 'https://',
+    canonical_scheme => 'https',
     canonical_host => 'bacds.org',
     events => \@events,
     highlight_special_types => 1,
