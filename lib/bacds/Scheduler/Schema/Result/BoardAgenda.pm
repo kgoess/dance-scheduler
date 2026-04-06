225da29311833332ebfe3d632bc20f672d43ffe0
Kevin M. Goess <kevin@goess.org>
2026-04-06 10:54:55 -0700

    Ran dbicdump to adjust ClaudeAI's schema files
    This way the next dbicdump will report only relevant changes.
    
    See the README.md for notes on dbicdump

--- lib/bacds/Scheduler/Schema/Result/BoardAgenda.pm
+++ lib/bacds/Scheduler/Schema/Result/BoardAgenda.pm
@@ -1,23 +1,112 @@
+use utf8;
 package bacds::Scheduler::Schema::Result::BoardAgenda;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+bacds::Scheduler::Schema::Result::BoardAgenda
+
+=cut
 
 use strict;
 use warnings;
+
 use base 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 TABLE: C<board_agenda>
+
+=cut
+
+__PACKAGE__->table("board_agenda");
+
+=head1 ACCESSORS
+
+=head2 board_agenda_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 meeting_date
+
+  data_type: 'date'
+  datetime_undef_if_invalid: 1
+  is_nullable: 0
+
+=head2 agenda_text
+
+  data_type: 'text'
+  default_value: ''''
+  is_nullable: 0
+
+=head2 created_ts
+
+  data_type: 'datetime'
+  datetime_undef_if_invalid: 1
+  is_nullable: 0
+
+=head2 modified_ts
+
+  data_type: 'timestamp'
+  datetime_undef_if_invalid: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "board_agenda_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "meeting_date",
+  { data_type => "date", datetime_undef_if_invalid => 1, is_nullable => 0 },
+  "agenda_text",
+  { data_type => "text", default_value => "''", is_nullable => 0 },
+  "created_ts",
+  {
+    data_type => "datetime",
+    datetime_undef_if_invalid => 1,
+    is_nullable => 0,
+  },
+  "modified_ts",
+  {
+    data_type => "timestamp",
+    datetime_undef_if_invalid => 1,
+    is_nullable => 0,
+  },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</board_agenda_id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("board_agenda_id");
+
+# Created by DBIx::Class::Schema::Loader v0.07052 @ 2026-04-06 10:50:29
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:NxYWVD/d6xtTCEJFm6Q6/w
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
 
 use Role::Tiny::With;
 with 'bacds::Scheduler::Schema::Role::AutoTimestamps';
-
-__PACKAGE__->load_components("InflateColumn::DateTime");
-__PACKAGE__->table("board_agenda");
-
-__PACKAGE__->add_columns(
-    board_agenda_id => { data_type => 'integer', is_auto_increment => 1 },
-    meeting_date    => { data_type => 'date', inflate_datetime => 1 },
-    agenda_text     => { data_type => 'text',    default_value => '' },
-    created_ts      => { data_type => 'datetime', inflate_datetime => 1 },
-    modified_ts     => { data_type => 'timestamp', inflate_datetime => 1 },
-);
-
-__PACKAGE__->set_primary_key('board_agenda_id');
 
 1;
